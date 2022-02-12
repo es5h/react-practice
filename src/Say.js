e69b8ffe54974f26d26cf1fe2395be20fdf846b7
Eunseok OH <lspk.lpg@gmail.com>
2022-02-12 21:43:59 +0900

    setState 연습

--- src/Say.js
+++ src/Say.js
@@ -4,12 +4,25 @@ const Say = () => {
   const [message, setMessage] = useState('');
   const onClickEnter = () => setMessage('hi');
   const onClickLeave = () => setMessage(' bye');
+  const [color, setColor] = useState('black');
 
   return (
     <div>
       <button onClick={onClickEnter}> enter</button>
       <button onClick={onClickLeave}> enter</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        {' '}
+        red{' '}
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        {' '}
+        green{' '}
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        {' '}
+        blue{' '}
+      </button>
     </div>
   );
 };
